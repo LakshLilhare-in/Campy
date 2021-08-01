fca7de11a8abcc485545d5ef0507350a247bd360
LakshLilhare-in <clean.lrlckids.safe.laksh@gmail.com>
2021-08-01 18:00:30 +0530

    -a

--- app.js
+++ app.js
@@ -160,7 +160,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
+app.listen(80, () => {
     console.log('Serving on port 3000')
 })
 
