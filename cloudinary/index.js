01b4432cb2e554648d91a6adf38f874cbfaf8c19
LakshLilhare-in <83571074+LakshLilhare-in@users.noreply.github.com>
2021-08-17 15:29:55 +0530

    Commit

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
     cloudinary.config({ 
-        cloud_name: 'dqxesl4nl', 
-        api_key: '391957664188372', 
-        api_secret: 'a25Cz--K8dgmxAkIV9M4C41BuT0' 
+        cloud_name: '', 
+        api_key: '', 
+        api_secret: '' 
     });
 
 const storage = new CloudinaryStorage({
